a488548c876942210fd80d1edfaca2b8a953a386
William Cheung <william.cheung.c@gmail.com>
2021-04-14 22:41:56 +0100

    added hook to fetch data using reducer

---
+++ src/hook/useFetch.ts
@@ -0,0 +1,60 @@
+import { Reducer, useEffect, useReducer } from "react"
+import { FetchEvent, FetchState } from "../model/fetch";
+import fetchReducer from "../reducer/fetchReducer";
+
+const baseUrl = process.env.REACT_APP_API_BASE_URL;
+
+const useFetch = <T>(
+  url: string,
+  typeGuard: (x: any) => x is T
+): FetchState<T> => {
+
+  const [state, dispatch] = useReducer<Reducer<FetchState<T>, FetchEvent<T>>>(
+    fetchReducer, 
+    {
+    data: null,
+    error: null,
+    loading: false
+  }
+  );
+
+  useEffect(
+    () => {
+      async function init() {
+          const response: Response = await fetch(baseUrl + url);
+
+          if(!response.ok) {
+            dispatch({
+              type: 'FetchFailed',
+              error: 'ResponseNotOk'
+            });
+            
+            return;
+          }
+
+            const json = await response.json();
+            
+            typeGuard(json)
+            ?      dispatch({
+                type: 'FetchSucceeded',
+                data: json
+              }) 
+              :
+              dispatch({
+                type: 'FetchFailed',
+                error: 'IncorrectResponseType'
+              })
+
+      }
+
+      init();
+    },
+    [
+      url
+    ]
+  )
+  
+  return state;
+}
+
+export default useFetch;
---
+++ src/model/fetch.ts
@@ -0,0 +1,67 @@
+/** 
+ * State 
+ */
+
+type ResponseNotOk = 'ResponseNotOk';
+
+export type IncorrectResponseType = 'IncorrectResponseType';
+
+export type FetchError
+ = ResponseNotOk 
+ | IncorrectResponseType;
+
+export interface FetchErrorState {
+  data: null;
+  error: FetchError;
+  loading: false;
+}
+
+export interface FetchingState {
+  data: null;
+  error: null;
+  loading: true;
+}
+
+export interface FetchedState<T> {
+  data: T;
+  error: null;
+  loading: false;
+}
+
+export interface FetchInitialState {
+  data: null;
+  error: null;
+  loading: false;
+}
+
+export type FetchState<T> 
+  = FetchInitialState 
+  | FetchingState
+  | FetchedState<T>
+  | FetchErrorState;
+
+/**
+ * Events
+ */
+
+export interface FetchSucceededEvent<T>  {
+    type: 'FetchSucceeded',
+    data: T
+};
+
+export interface FetchFailedEvent {
+  type: 'FetchFailed',
+  error: FetchError
+};
+
+export type FetchEvent<T> 
+  = FetchSucceededEvent<T>
+  | FetchFailedEvent
+
+/** 
+ * Type Guards
+ */
+
+export const isFetchingState = (state: FetchState<any>): state is FetchingState => {
+  return state.data === null && state.error === null && state.loading === true;
+}
